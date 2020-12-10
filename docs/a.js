3c7228c2541ed2d134cb7c7cdffa76ec910d7a2d
디지털토이즈 <pepsi2@gmail.com>
2020-12-10 23:39:49 +0900

    Update a.js

--- docs/a.js
+++ docs/a.js
@@ -1,10 +1,28 @@
 var result = undefined;
+
+Object.prototype.equals = function(x) {
+	// 인자값의 Type이 object가 아닐경우 false를 리턴한다.
+	if(typeof x !== "object") return false;
+	// Type을 String으로 변환한다.
+	var arr1 = JSON.stringify(this);
+	var arr2 = JSON.stringify(x);
+
+	return (arr1 === arr2);
+}
+
 function onRunClick() {
   result = undefined;
   let param = document.getElementById("param").value || '';
   eval('param = '+param);
   let func = eval('(param)=>{'+document.getElementById("func").value+'}');
+  
   result = func(param);
-  document.getElementById("resultmsg").innerHTML = (result == eval(document.getElementById("require").innerText))? 'Success':'Fail';
+  let target = eval(document.getElementById("require").innerText);
+  
+  if(typeof result == "object") {
+    document.getElementById("resultmsg").innerHTML = result.equals(target)? 'Success':'Fail';
+  } else {
+    document.getElementById("resultmsg").innerHTML = result === target? 'Success':'Fail';
+  }
   document.getElementById("result").innerHTML = result || '';
 }
