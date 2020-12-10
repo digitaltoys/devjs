34bb865297c0f1c8bfb0f4b513e9fb6a0ca5c5e3
디지털토이즈 <pepsi2@gmail.com>
2020-12-11 00:05:22 +0900

    Update a.js

--- docs/a.js
+++ docs/a.js
@@ -1,4 +1,6 @@
 var result = undefined;
+document.getElementById("param").value = getCookie('param');
+document.getElementById("func").value = getCookie('func');
 
 Object.prototype.equals = function(x) {
 	// 인자값의 Type이 object가 아닐경우 false를 리턴한다.
@@ -9,12 +11,37 @@ Object.prototype.equals = function(x) {
 
 	return (arr1 === arr2);
 }
+function setCookie(cookie_name, value, days) {
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + days);
+  // 설정 일수만큼 현재시간에 만료값으로 지정
+
+  var cookie_value = escape(value) + ((days == null) ? '' : '; expires=' + exdate.toUTCString());
+  document.cookie = cookie_name + '=' + cookie_value;
+}
+
+function getCookie(cookie_name) {
+  var x, y;
+  var val = document.cookie.split(';');
+
+  for (var i = 0; i < val.length; i++) {
+    x = val[i].substr(0, val[i].indexOf('='));
+    y = val[i].substr(val[i].indexOf('=') + 1);
+    x = x.replace(/^\s+|\s+$/g, ''); // 앞과 뒤의 공백 제거하기
+    if (x == cookie_name) {
+      return unescape(y); // unescape로 디코딩 후 값 리턴
+    }
+  }
+}
 
 function onRunClick() {
   result = undefined;
   let param = document.getElementById("param").value || '';
   eval('param = '+param);
-  let func = eval('(param)=>{'+document.getElementById("func").value+'}');
+  let func = document.getElementById("func").value;
+  setCookie('param', param, 9999999);
+  setCookie('func', func, 9999999);
+  func = eval('(param)=>{'+func+'}');
   
   result = func(param);
   let target = eval(document.getElementById("require").innerText);
