926c26270f554ec110ecc8a6092688634aafdd13
디지털토이즈 <pepsi2@gmail.com>
2020-12-10 14:24:59 +0900

    Create a.js

---
+++ docs/a.js
@@ -0,0 +1,8 @@
+let param = document.getElementById("param").value;
+let func;
+function onRunClick() = {
+  func = eval('(param)=>{'+document.getElementById("func").value+'}');
+  console.log(param,func);
+  console.log(document.getElementById("func").value);
+  document.getElementById("result").innerHTML = func(param);
+}
