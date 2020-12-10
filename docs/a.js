ee9e248912a8b4048316239ded210d669a8f9a23
디지털토이즈 <pepsi2@gmail.com>
2020-12-10 22:38:04 +0900

    Update a.js

--- docs/a.js
+++ docs/a.js
@@ -1,5 +1,5 @@
 function onRunClick() {
-  let param = eval(document.getElementById("param").value) || '';
+  let param = document.getElementById("param").value || '';
   let func = eval('(param)=>{'+document.getElementById("func").value+'}');
-  document.getElementById("result").innerHTML = func(param) || '';
+  document.getElementById("result").innerHTML = func(eval(param)) || '';
 }
